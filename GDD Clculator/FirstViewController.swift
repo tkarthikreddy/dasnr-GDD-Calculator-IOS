59c6330b048ddf74bd24278fdb2dc049c7598d7e
Karthik Talakanti <karthik.talakanti@gmail.com>
2020-01-19 00:18:38 -0600

    Initial Commit

---
+++ GDD Clculator/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  GDD Clculator
+//
+//  Created by Karthik Talakanti on 1/19/20.
+//  Copyright © 2020 Karthik Talakanti. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
